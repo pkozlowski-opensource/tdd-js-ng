aa05872462bd96778b8a143fd958f32be2f4b7f1
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-12-06 19:35:54 +0100

    wip

---
+++ src/04_modules_and_di/users.spec.js
@@ -0,0 +1,90 @@
+describe('users controller', function () {
+
+  var $scope, $controller;
+  var userStorage;
+
+  beforeEach(module('users'));
+  // let's load real dependencies so those are called if not mocked
+  beforeEach(module('userstorage'));
+  beforeEach(inject(function (_$rootScope_, _$controller_, _UserStorage_) {
+    $scope = _$rootScope_;
+    // now it should be more obvious why we use underscores
+    $controller = _$controller_;
+    userStorage = _UserStorage_;
+  }));
+
+  it('should initialize scope with an empty users collection', function () {
+    usersCtrl = $controller('UsersCtrl', {
+      '$scope': $scope
+    });
+    expect($scope.user).toEqual({});
+    expect($scope.users.length).toEqual(0);
+  });
+
+  it('should save a current user and update users list', function () {
+
+    var savedUser;
+
+    usersCtrl = $controller('UsersCtrl', {
+      '$scope': $scope,
+      // ad-hoc stubbing - I can override individual dependencies
+      'UserStorage' : {
+        save: function(user) {
+          return savedUser = user;
+        },
+        getAll: function() {
+          return [savedUser];
+        }
+      }
+    });
+
+    $scope.user = {name : 'foo'};
+
+    //here we were not really testing that a user was saved in a back-end
+    $scope.save();
+
+    expect($scope.users.length).toEqual(1);
+    expect($scope.user).toEqual({});
+  });
+
+  it('should dirty check and allow clearing user edits', function () {
+    usersCtrl = $controller('UsersCtrl', {
+      '$scope': $scope
+    });
+
+    $scope.user.name = 'bar';
+    expect($scope.hasEdits()).toBeTruthy();
+
+    $scope.clear();
+    expect($scope.hasEdits()).toBeFalsy();
+  });
+
+  it('should remove a selected user', function () {
+
+    //mocking with Jasmine spies
+    spyOn(userStorage, 'remove');
+    spyOn(userStorage, 'getAll').andReturn([]);
+
+    usersCtrl = $controller('UsersCtrl', {
+      '$scope': $scope
+    });
+    $scope.users = [{id: 1, name: 'foo'}];
+
+    $scope.remove($scope.users[0].id);
+
+    expect($scope.users.length).toEqual(0);
+  });
+
+  it('should support editing users', function () {
+
+    usersCtrl = $controller('UsersCtrl', {
+      '$scope': $scope
+    });
+    $scope.users = [{id: 1, name: 'foo'}];
+
+    $scope.edit($scope.users[0]);
+
+    expect($scope.user.name).toEqual('foo');
+  });
+
+});
---
+++ src/04_modules_and_di/userstorage.spec.js
@@ -0,0 +1,55 @@
+describe('User storage as global function', function () {
+
+  var userStorage;
+
+  // load services defined in a given modules
+  beforeEach(module('userstorage'));
+  beforeEach(inject(function (_UserStorage_) {
+    userStorage = _UserStorage_;
+  }));
+
+  // remaining tests are unchanged, we've only modified instantiation method
+  describe('basic CRUD operations', function () {
+
+    it('should allow adding and querying users by id', function () {
+      var user = userStorage.save({
+        name: 'Pawel'
+      });
+
+      expect(userStorage.getById(user.id).name).toEqual('Pawel');
+    });
+
+    it('should allow querying all users', function () {
+
+      userStorage.save({name: 'foo'});
+      userStorage.save({name: 'bar'});
+
+      expect(userStorage.getAll().length).toEqual(2);
+    });
+
+    it('should support removing users by id', function () {
+
+      // setup
+      userStorage.save({id: '1', value: 'foo'});
+      userStorage.save({id: '2', value: 'bar'});
+
+      // stimulus
+      var removed = userStorage.remove('1');
+
+      // verify expectations
+      expect(removed.value).toEqual('foo');
+      expect(userStorage.getById('1')).toBeFalsy();
+      expect(userStorage.getById('2').value).toEqual('bar');
+    });
+
+  });
+
+  describe('corner cases', function () {
+
+    it('should return null for non existing users', function () {
+      expect(userStorage.getById('foo')).toBeNull();
+    });
+
+  });
+
+});
