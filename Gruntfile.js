fa73f29da10daacf7b484c681aafba32de5e078e
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-12-08 18:28:03 +0100

    chore: add generator for Jasmine HTML test runner

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,8 @@
 module.exports = function (grunt) {
 
+  var path = require('path');
+  var s = require('string');
+
   grunt.initConfig({
     jshint: {
       files: [
@@ -50,8 +53,49 @@ module.exports = function (grunt) {
   grunt.registerTask('tdd', ['karma:tdd']);
   grunt.registerTask('www', ['connect:server']);
 
+  // ========== Internal code generation tasks: Jasmine ==================
 
+  grunt.registerTask('prepareJasmine', function () {
+
+    console.log('Preparing Jasmine tests for: ');
+
+    //for each folder in /src
+    grunt.file.expand('src/*').forEach(function (exercisePath) {
+
+      var indexPath = exercisePath + path.sep + 'index.html';
+      var codeFiles = [];
+      var specFiles = [];
+
+      //check if index.html doesn't exist
+      if (grunt.file.exists(indexPath)) {
+        return;
+      }
+
+      console.log(exercisePath);
+
+      // prepare index.html with Jasmine tests for other exercises
+      //  - list files in a given folder
+      //  - sort those for specs / non specs
+      grunt.file.expand(exercisePath + path.sep + '*.js').forEach(function (exerciseFilePath) {
+        (s(exerciseFilePath).endsWith('spec.js') ? specFiles : codeFiles)
+          .push(path.basename(exerciseFilePath));
+      });
+
+      // process a template with a Jasmine index.html
+      grunt.file.write(indexPath, grunt.template.process(grunt.file.read('grunt/jasmine.index.tpl.html'), {
+        data: {
+          files: {
+            code: codeFiles,
+            specs: specFiles
+          }
+        }
+      }));
+    });
+  });
+
+  // load standard Grunt tasks
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-connect');
   grunt.loadNpmTasks('grunt-karma');
+
 };
