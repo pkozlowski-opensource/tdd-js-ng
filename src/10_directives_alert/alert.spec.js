86707496966449acf04f002edb02c6ecd9f78aec
Pawel Kozlowski <pkozlowski.opensource@gmail.com>
2013-12-16 21:24:21 +0100

    feat: alert directive

---
+++ src/10_directives_alert/alert.spec.js
@@ -0,0 +1,54 @@
+describe("alert", function () {
+
+  var $scope, $compile;
+
+  beforeEach(module('bs.alert'));
+  beforeEach(module('templates'));
+
+  beforeEach(inject(function ($rootScope, _$compile_) {
+    $scope = $rootScope;
+    $compile = _$compile_;
+  }));
+
+  function compileElement(elementString, scope) {
+    var element = $compile(elementString)(scope);
+    scope.$digest();
+    return element;
+  }
+
+  function findCloseButton(element) {
+    return element.find('button.close');
+  }
+
+  it('should set "warning" CSS class by default', function () {
+    var element = compileElement('<bs-alert>Default</bs-alert>', $scope);
+    expect(element.find('div.alert')).toHaveClass('alert-warning');
+  });
+
+  it('should set appropriate CSS class based on the alert type', function () {
+    //ex:start
+    var element = compileElement('<bs-alert type="{{\'info\'}}">Info</bs-alert>', $scope);
+    expect(element.find('div.alert')).toHaveClass('alert-info');
+    //ex:end
+  });
+  
+  it('should not show close buttons if no close callback specified', function () {
+    //ex:start
+    var element = compileElement('<bs-alert>No close</bs-alert>', $scope);
+    expect(findCloseButton(element).is(':visible')).toBeFalsy();
+    //ex:end
+  });
+
+  it('should fire callback when closed', function () {
+    //ex:start
+    $scope.removeAlert = function() {
+      $scope.removed = true;
+    };
+    var element = compileElement('<bs-alert close="removeAlert()">Has close</bs-alert>', $scope);
+
+    findCloseButton(element).click();
+    expect($scope.removed).toBeTruthy();
+    //ex:end
+  });
+
+});
